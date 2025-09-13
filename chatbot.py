cee384125c9057f6a6fd058ef4db2f3bb2ddbb12
mateoKZ <mcesphera@gmail.com>
2025-09-13 13:36:38 -0300

    feat: Actualiza al modelo gemini-2.5-flash

--- chatbot.py
+++ chatbot.py
@@ -21,7 +21,7 @@ safety_settings = [
   { "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
   { "category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
 ]
-model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest",
+model = genai.GenerativeModel(model_name="gemini-2.5-flash",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 # -------------------------
