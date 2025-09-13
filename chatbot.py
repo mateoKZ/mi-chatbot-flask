7de6fc04e28a050b1838df3825440b5fca0175a2
mateoKZ <mcesphera@gmail.com>
2025-09-13 12:39:06 -0300

    fix: Corrige el nombre del modelo de Gemini a gemini-pro

--- chatbot.py
+++ chatbot.py
@@ -21,7 +21,7 @@ safety_settings = [
   { "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
   { "category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
 ]
-model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+model = genai.GenerativeModel(model_name="gemini-pro",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 # -------------------------
